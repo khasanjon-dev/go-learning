a2fb84f28c3f5da9eb56b525292753ce89916e51
khasanjon <khasanjon.dev@gmail.com>
2024-02-17 17:33:09 +0500

    for inside for usage

--- learning.go
+++ learning.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 5; i++ {
-		if i == 3 {
-			break
+	adj := [2]string{"big", "tasty"}
+	fruits := [3]string{"apple", "orange", "banana"}
+	for i := 0; i < len(adj); i++ {
+		for j := 0; j < len(fruits); j++ {
+			fmt.Println(adj[i], fruits[j])
 		}
-		fmt.Println(i)
 	}
 }
