f0ced7f3ab34928e640addb4fc635fc225a918c7
khasanjon <khasanjon.dev@gmail.com>
2024-02-17 17:55:21 +0500

    add some

---
+++ func_arguments_parametr.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func familyName(fname string) {
+	fmt.Println("Hello", fname)
+
+}
+func familyAges(firstName string, age int) {
+	fmt.Println("Hello", age, "year old", firstName)
+
+}
+
+func main() {
+	//familyName("Khasan")
+	familyAges("Khasan", 21)
+}
